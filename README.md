68c16da951595b3034619d2c67a86dfac6a1e365
Max Gasser <mrgasser@ucsc.edu>
2021-02-22 16:14:13 -0800

    adding starting files

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # chess
 Chess game with chess engine
+
+credits
+
+chess board:
+https://stackoverflow.com/questions/4954395/create-board-game-like-grid-in-python
+
+chess library:
+https://python-chess.readthedocs.io/en/latest/index.html
---
+++ chess_prgm.py
@@ -0,0 +1,14 @@
+import chess
+import chess.svg
+
+# from IPython.display import SVG
+# SVG(chess.svg.board(board=board,size=400))
+
+def eval_board(board):
+    
+
+if __name__ == "__main__":
+    board = chess.Board()
+
+    print(board)
+    print(board.legal_moves)
